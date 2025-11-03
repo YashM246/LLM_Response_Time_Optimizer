72e734c506184c1171e82e7ad3d13e06f3455098
Yash <94853015+YashM246@users.noreply.github.com>
2025-11-02 21:33:26 -0800

    Completed Implementation of KV Caching

--- src/kv_cache.py
+++ src/kv_cache.py
@@ -17,14 +17,46 @@ def initialize_cache(num_layers:int,
     #   ...
     #   Till Layer n
     # }
-    pass
+    cache = {}
+
+    # Create cache for each layer
+    for layer_idx in range(num_layers):
+        cache[layer_idx] = {
+            'key': jnp.zeros((batch_size, num_heads, max_seq_len, head_dim), dtype=dtype),
+            'value':jnp.zeros((batch_size, num_heads, max_seq_len, head_dim), dtype=dtype)
+        }
+
+    return cache
 
 def update_cache(cache, layer_idx, new_keys, new_values, cache_position):
     # Extract current arrays
     # Then insert new keys/values at cache position
     # (JAX Functional Programming) -> Create new cache dict, not modifying in place
     # Returns new cache
-    pass
+    
+    current_keys = cache[layer_idx]['key']
+    current_values = cache[layer_idx]['value']
+
+    updated_keys = jax.lax.dynamic_update_slice(
+        current_keys,
+        new_keys,
+        (0, 0, cache_position, 0)   # Start at the position cache_position
+        )
+    
+    updated_values = jax.lax.dynamic_update_slice(
+        current_values,
+        new_values,
+        (0, 0, cache_position, 0)
+    )    
+    
+    # Create new cache dict
+    new_cache = cache.copy()
+    new_cache[layer_idx] = {
+        'key': updated_keys,
+        'value': updated_values
+    }
+
+    return new_cache
 
 def get_cached_kv(cache: Dict[int, Dict[str, jnp.ndarray]],
                   layer_idx: int,
@@ -32,4 +64,12 @@ def get_cached_kv(cache: Dict[int, Dict[str, jnp.ndarray]],
     # The cache will always be of length max_seq_len
     # But we might've only filled cache_length positions so far
     # Extract full cache, but return only sliced version uptill filled positions
-    pass
+    
+    full_keys = cache[layer_idx]['keys']
+    full_values = cache[layer_idx]['value']
+
+    # Shape: [batch, num_heads, cache_length, head_dim]
+    keys = full_keys[:, :, :cache_length, :]
+    values = full_values[:, :, :cache_length, :]
+
+    return keys, values
